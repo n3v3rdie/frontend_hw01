1c06f5842a7bc1b861f5d621136488d6c0bb4296
Nikolay <n3v3r_die@mail.ru>
2020-07-30 16:57:32 +0300

    Added lazy-load, preload, preconnect

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,18 @@ import initPopups from './parts/popups';
 import initSliders from './parts/sliders';
 import initValidator from './parts/validator'
 
+const lazyLoad = () => {
+  $('.js-lazy').each((index, el) => {
+    const $image = $(el);
+    const realSrc = $image.attr('data-src');
+    console.log(realSrc);
+    $image.attr('src', realSrc);
+  });
+}
+
 $(document).ready(() => {
   initPopups();
   initSliders();
   initValidator();
+  lazyLoad();
 });
